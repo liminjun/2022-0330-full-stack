a8b97ffed6b31ab0ae87bdb4792ba7fbf90d8cbb
liminjun <v-leeli@hotmail.com>
2022-10-16 21:22:36 +0800

    feat:添加week22示例代码

---
+++ week22/faker-demo/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+var bodyParser = require('body-parser');
+var faker = require('faker');
+
+const app = express()
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+  const user = {
+    'randomName': faker.name.findName(),
+    'randomEmail': faker.internet.email(),
+    'randomPhone': faker.phone.phoneNumber()
+  }
+  res.send('Hello World'+ JSON.stringify(user))
+})
+
+app.post('/test', function (req, res) {
+    console.log(req.body)
+    console.log(req.body.username, req.body.password);
+    res.send(req.body.username + ':'+ req.body.password)
+})
+
+app.listen(3000)
+console.log('Server listening port:3000')
